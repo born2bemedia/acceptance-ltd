4f0148fc22e8320ba7743c1cdd042d656bb90fb9
SergWebAcceptance <serg.web@acceptance.ltd>
2024-09-30 16:53:28 +0300

    Added popup to homepage

--- src/app/(home)/_components/HomeHero.jsx
+++ src/app/(home)/_components/HomeHero.jsx
@@ -2,31 +2,35 @@
 import Link from "next/link";
 import React from "react";
 import Image from "next/image";
-
+import RequestButton from "@/global_components/RequestButton";
 
 const HomeHero = () => {
-    return (
-      <section className="home-hero">
-        <div className="home-hero__inner container">
-            <div className="home-hero__top">
-                <h1>Expert Support for International Business Operations</h1>
+  return (
+    <section className="home-hero">
+      <div className="home-hero__inner container">
+        <div className="home-hero__top">
+          <h1>Expert Support for International Business Operations</h1>
 
-                <div className="content">
-                    <p>Compliance, legal, and financial solutions for efficient global operations. Get expert help navigating regulations, managing financial flows, and optimising your business.</p>
-                    <Link href="#" className="button">Request a Consultation</Link>
-                </div>
-            </div>
-
-            <Image
-                src="/images/hero-image.webp"
-                quality={100}
-                width={1704}
-                height={450}
-                alt="Hero image"
-            />
+          <div className="content">
+            <p>
+              Compliance, legal, and financial solutions for efficient global
+              operations. Get expert help navigating regulations, managing
+              financial flows, and optimising your business.
+            </p>
+            <RequestButton text={"Request a Consultation"} />
+          </div>
         </div>
-      </section>
-    );
-  };
-  
-  export default HomeHero;
+
+        <Image
+          src="/images/hero-image.webp"
+          quality={100}
+          width={1704}
+          height={450}
+          alt="Hero image"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default HomeHero;
