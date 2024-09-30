438bfc240e316e34b90ce79efbc745195be4852f
SergWebAcceptance <serg.web@acceptance.ltd>
2024-09-30 18:26:28 +0300

    Fixed expert block

--- src/global_components/ExpertSupport.jsx
+++ src/global_components/ExpertSupport.jsx
@@ -7,12 +7,8 @@ const ExpertSupport = ({ title, text }) => {
   return (
     <section className="expert-support">
       <div className="expert-support__inner container">
-        <h2>Get expert support for your business</h2>
-        <p>
-          Ready to navigate the complexities of compliance, legal services, and
-          financial advisory? Contact us today for a consultation tailored to
-          your industry needs.
-        </p>
+        <h2>{title}</h2>
+        <p>{text}</p>
         <RequestButton />
       </div>
     </section>
