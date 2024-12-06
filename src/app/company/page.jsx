a67dc6ab87aa624b0e47bfa99a6ddb3f75112178
Dmitry Dialen <104232574+dmitrydialen@users.noreply.github.com>
2024-12-06 18:13:28 +0200

    update company page

--- src/app/company/page.jsx
+++ src/app/company/page.jsx
@@ -1,9 +1,17 @@
 import Image from "next/image";
 import "@/style/company.scss";
 import CompanyHero from "./components/CompanyHero";
+import WhyChoseUs from "./components/WhyChoseUs";
+import OurBlock from "./components/OurBlock";
+import RequestPopup from "@/global_components/RequestPopup";
 
 export default function company() {
     return (
-        <CompanyHero />
+        <>
+            <CompanyHero />
+            <WhyChoseUs />
+            <OurBlock />
+            <RequestPopup />
+        </>
     );
 }
