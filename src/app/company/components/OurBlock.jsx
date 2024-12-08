d35ad1478d059cbaef8a6ff139b9c16a2384b9a7
Dmitry Dialen <104232574+dmitrydialen@users.noreply.github.com>
2024-12-08 19:05:15 +0200

    Update FAQ page

--- src/app/company/components/OurBlock.jsx
+++ src/app/company/components/OurBlock.jsx
@@ -1,7 +1,5 @@
 "use client";
 import Image from "next/image";
-import Link from "next/link";
-import { ButtonIcon } from '../../../global_components/Icons';
 import RequestButton from "@/global_components/RequestButton";
 
 const OurBlock = () => {
