84e338c24f6bde33c4c327a2c292c29b965a0c1d
Dmitry Dialen <104232574+dmitrydialen@users.noreply.github.com>
2024-12-11 05:54:35 +0200

    update

--- src/app/contact-us/components/ContactForm.jsx
+++ src/app/contact-us/components/ContactForm.jsx
@@ -34,7 +34,7 @@ const ContactForm = () => {
 
   const handleSubmit = async (values, { setSubmitting, resetForm, setStatus }) => {
     try {
-      const response = await fetch("/api/contact", {
+      const response = await fetch("/contexts/contact", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
