80abfd729183c5176f0fc7daea57647ac303d37d
SergWebAcceptance <serg.web@acceptance.ltd>
2025-10-28 12:44:54 +0200

    Update images and components: Replace outdated image sources in HomeHero, Contact Us, and FAQ sections. Modify ExpertSupport component to render HTML content safely. Update background image in insights.scss for improved visual consistency.

--- src/global_components/ExpertSupport.jsx
+++ src/global_components/ExpertSupport.jsx
@@ -7,8 +7,8 @@ const ExpertSupport = ({ title, text }) => {
   return (
     <section className="expert-support">
       <div className="expert-support__inner container">
-        <h2 className="fadeInUp">{title}</h2>
-        <p className="fadeInUp">{text}</p>
+        <h2 className="fadeInUp" dangerouslySetInnerHTML={{ __html: title }} />
+        <p className="fadeInUp" dangerouslySetInnerHTML={{ __html: text }} />
         <RequestButton />
       </div>
     </section>
